c643de539862260d01ee661043d93cd7c149f404
Michael Mathioudakis <michael.mathioudakis@hiit.fi>
2015-09-10 23:45:00 +0300

    cleaning up the code

--- bertrand_helpers.py
+++ bertrand_helpers.py
@@ -44,28 +44,38 @@ def plot_chord_length_distribution(ax, lengths, R, solution_pct,
                                       color = 'black', linewidth = 2)
     return _tmp
 
-def plot_sample_chords(ax, solution_coord_a, solution_coord_b):
+def plot_sample_chords(ax, solution_coord_a, solution_coord_b, R):
 
 	M = len(solution_coord_b)
 	assert(M == len(solution_coord_b))
+
 	# construct the chords from the coordinates of their endpoints
 	chords_A = [patches.PathPatch(patches.Path([solution_coord_a[i],
 		solution_coord_b[i]], [patches.Path.MOVETO, patches.Path.LINETO]),
 		edgecolor = "blue", lw = 0.5, fill = False) for i in range(M)]
 
-	_tmp = ax.set(xlim = (-1.1, 1.1), ylim = (-1.1, 1.1), aspect = 1)
-	_tmp = ax.axis('off')
+	_tmp = ax.set(xlim = (-R - 0.01, R + 0.01),
+		ylim = (-R - 0.01, R + 0.01), aspect = 1)
 
-	# plot the triangle and the chords
+	# plot the chords
 	for chord in chords_A:
 	    _tmp = ax.add_patch(chord)
-	triangle = patches.RegularPolygon((0,0), 3, 1, lw = 3, fill = False)
+	return _tmp
+
+def plot_triangle(ax, R):
+	triangle = patches.RegularPolygon((0,0), 3, R, lw = 3, fill = False)
 	_tmp = ax.add_patch(triangle)
+	return _tmp
+
+def plot_circle(ax, c, r, alpha):
+	circle = patches.Circle(c, r, ec = 'black', fc = "grey", alpha = alpha)
+	_tmp = ax.add_patch(circle)
 	return _tmp
 
 def plot_length_distr_and_chords(lengths, R, solution_pct,
 			triangle_edge_length, solution_coord_a, solution_coord_b, N):
 	fig, ax = plt.subplots(1, 2, figsize = (12, 5))
+	for i in range(len(ax)): ax[i].axis('off')
 
 	# plot distribution of chord lengths
 	_tmp = plot_chord_length_distribution(ax[0], lengths, R,
@@ -75,10 +85,53 @@ def plot_length_distr_and_chords(lengths, R, solution_pct,
 	# plot sample of M (out of N) chords
 	M = min(500, N)
 	_tmp = plot_sample_chords(ax[1], solution_coord_a[:M],
-										solution_coord_b[:M])
+									solution_coord_b[:M], R)
+	_tmp = plot_triangle(ax[1], R)
 
 	return
 
+def plot_chords_and_small_circle(c, r, R, N, sol_A_coord_a, sol_A_coord_b,
+								sol_B_coord_a, sol_B_coord_b,
+								sol_C_coord_a, sol_C_coord_b):
+	fig, ax = plt.subplots(1, 3, figsize = (15, 5))
+	for i in range(len(ax)): ax[i].axis('off')
+
+	M = min(500, N)
+	_tmp = plot_sample_chords(ax[0], sol_A_coord_a[:M], sol_A_coord_b[:M], R)
+	_tmp = plot_circle(ax[0], c, r, 0.75)
+
+	_tmp = plot_sample_chords(ax[1], sol_B_coord_a[:M], sol_B_coord_b[:M], R)
+	_tmp = plot_circle(ax[1], c, r, 0.75)
+
+	_tmp = plot_sample_chords(ax[2], sol_C_coord_a[:M], sol_C_coord_b[:M], R)
+	_tmp = plot_circle(ax[2], c, r, 0.75)
+
+	return
+
+def plot_solution_for_small_vs_large(solution_A_pct, sol_A_small_pct,
+	solution_B_pct, sol_B_small_pct, solution_C_pct, sol_C_small_pct):
+
+	fig, ax = plt.subplots(1, 3, figsize = (15, 5))
+
+	_tmp = ax[0].set(xlim = (-0.5, 1.5), ylim = (0,100), xticks = [0, 1],
+	                 xticklabels = ['Large', 'Small'],
+	                 yticks = [0, int(solution_A_pct), 100])
+	_tmp = ax[0].bar([0, 1], [solution_A_pct, sol_A_small_pct], width = 0.5,
+	                align = 'center', color = "blue")
+
+	_tmp = ax[1].set(xlim = (-0.5, 1.5), ylim = (0,100), xticks = [0, 1],
+	                 xticklabels = ['Large', 'Small'], 
+	                 yticks = [0, int(solution_B_pct), 100])
+	_tmp = ax[1].bar([0, 1], [solution_B_pct, sol_B_small_pct], width = 0.5,
+	                align = 'center', color = "red")
+	                 
+	_tmp = ax[2].set(xlim = (-0.5, 1.5), ylim = (0,100), xticks = [0, 1],
+	                 xticklabels = ['Large', 'Small'], 
+	                 yticks = [0, int(solution_C_pct), 100])
+	_tmp = ax[2].bar([0, 1], [solution_C_pct, sol_C_small_pct], width = 0.5,
+	                align = 'center', color = "green")
+
+	return
 
 ####
 
